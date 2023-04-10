50641c610dd4c23e73e53b44b0e136ee59854ddb
ebomb07 <66098261+ebomb07@users.noreply.github.com>
2023-04-09 20:12:59 -0400

    Fixed bug with keyboard input

--- backend/setup.py
+++ backend/setup.py
@@ -1,22 +1,25 @@
 import PySimpleGUI as sg
+import os
 
-
+KeyboardInterrupt = True
 sg.theme('DarkGrey8')   # Add a touch of color
 # All the stuff inside your window.
 layout = [  [sg.Push(), sg.Text('Welcome Dath Password locker', justification='c'), sg.Push()],
-            [sg.Text("Select files or folder:"), sg.Input(key='-IN1-',size=(40, 10),enable_events=True, readonly=True,background_color='#ffd22b',text_color='Black'),sg.FolderBrowse('Select', enable_events=True)],
+          
+            [sg.Push(),sg.Text("Select files or folder:"), sg.Input(key='-IN1-',size=(40, 10),enable_events=True, readonly=True,background_color='#ffd22b',text_color='Black'),sg.FolderBrowse('Select', enable_events=True),sg.Push()],
+            
             [sg.VPush()],
             [sg.Push(), sg.Button('Ok', disabled=True, key='okB',size=(20,5)), sg.Button('Cancel', size=(20,5)), sg.Push() ]]
 
 
 
 # Create the Window
-window = sg.Window('Dath Password Project', layout, icon="backend/dath_logo.ico", size= [500, 500])
+window = sg.Window('Dath Password Project', layout, icon="backend/dath_logo.ico", size= [750, 500])
 
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
-    if window['-IN1-'] != "":
+    if os.path.exists(values['Select']):
         window['okB'].update(disabled=False)
     if event == 'okB':
         
